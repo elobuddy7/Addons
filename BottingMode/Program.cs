9e4ffa8c20137bd8f7dd219fcc0eff38898ab476
EloBuddy7 <3fd1vi+d8wj7kkww946g@sharklasers.com>
2016-03-17 19:28:43 +0900

    Added files via upload

---
+++ BottingMode/Program.cs
@@ -0,0 +1,21 @@
+﻿using EloBuddy;
+
+namespace BottingMode
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Hacks.DisableTextures = true;
+            Hacks.AntiAFK = true;
+            Hacks.RenderWatermark = false;
+
+            ManagedTexture.OnLoad += ManagedTexture_OnLoad;
+        }
+
+        private static void ManagedTexture_OnLoad(OnLoadTextureEventArgs args)
+        {
+            args.Process = false;
+        }
+    }
+}
