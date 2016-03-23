2a769dd5649befc7a051744c5963259f2d5f812e
EloBuddy7 <3fd1vi+d8wj7kkww946g@sharklasers.com>
2016-03-23 17:49:52 +0900

    Added files via upload

---
+++ How many times do i click/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+
+using EloBuddy;
+
+namespace How_many_times_do_i_click
+{
+    internal static class Program
+    {
+        private static int Clicks;
+
+        private static void Main(string[] args)
+        {
+            Player.OnIssueOrder += Player_OnIssueOrder;
+            Drawing.OnDraw += Drawing_OnDraw;
+        }
+
+        private static void Player_OnIssueOrder(Obj_AI_Base sender, PlayerIssueOrderEventArgs args)
+        {
+            if (sender.IsMe && (args.Order.HasFlag(GameObjectOrder.MoveTo) || args.Order.HasFlag(GameObjectOrder.AttackTo)))
+            {
+                Clicks++;
+            }
+        }
+
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            Drawing.DrawText(Drawing.Height * 0.1f, Drawing.Width * 0.1f, Color.GreenYellow, $"Clicks: {Clicks}");
+        }
+    }
+}
---
+++ How many times do i click/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("How many times do i click")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("How many times do i click")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("236c2635-f1fe-4961-a257-d662c537bda2")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
